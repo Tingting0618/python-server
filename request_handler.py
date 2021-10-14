c27d41d575910a21e4a57cc0bdce8e797f224dfa
duan.tingting@yahoo.com <tingtingduan@tingtings-Air.attlocal.net>
2021-10-14 07:05:56 -0500

    chapter8 problem

--- request_handler.py
+++ request_handler.py
@@ -1,5 +1,5 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_single_animal,create_animal
+from animals import get_all_animals, get_single_animal,create_animal,delete_animal,update_animal
 import json
 
 # Here's a class. It inherits from another class.
@@ -68,6 +68,36 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         # Encode the new animal and send in response
         self.wfile.write(f"{new_animal}".encode())
+    
+    def do_DELETE(self):
+        # Set a 204 response code
+        self._set_headers(204)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            delete_animal(id)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
+
+    def do_PUT(self):
+        self._set_headers(204)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            update_animal(id, post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
 
     def parse_url(self, path):
         # Just like splitting a string in JavaScript. If the
