55698bfa08793b217971b8636e24dc39982ba4eb
Tingting Duan <duan.tingting@yahoo.com>
2021-10-21 07:30:56 -0500

    chapter 6 completed

--- customers/__init__.py
+++ customers/__init__.py
@@ -2,4 +2,5 @@ from .request import (get_all_customers,
                       get_customers_by_email, 
                       get_single_customer,
                       create_customer,
-                      delete_customer)
+                      delete_customer,
+                      update_customer)
--- request_handler.py
+++ request_handler.py
@@ -1,10 +1,27 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_single_animal,create_animal,delete_animal,update_animal
-from customers import get_all_customers,get_customers_by_email,get_single_customer,delete_customer
-from customers.request import create_customer
-from locations import get_all_locations,get_single_location,create_location,delete_location
-from employees import get_all_employees,get_single_employee,create_employee,delete_employee
 import json
+from animals import (get_all_animals,
+                     get_single_animal,
+                     create_animal,
+                     delete_animal,
+                     update_animal)
+from customers import (get_all_customers,
+                       get_customers_by_email,
+                       get_single_customer,
+                       delete_customer,
+                       update_customer,
+                       create_customer)
+from locations import (get_all_locations,
+                       get_single_location,
+                       create_location,
+                       delete_location,
+                       update_location)
+from employees import (get_all_employees,
+                       get_single_employee,
+                       create_employee,
+                       delete_employee,
+                       update_employee)
+
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -23,10 +40,41 @@ class HandleRequests(BaseHTTPRequestHandler):
     def do_OPTIONS(self):
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-        self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
+        self.send_header('Access-Control-Allow-Methods',
+                         'GET, POST, PUT, DELETE')
+        self.send_header('Access-Control-Allow-Headers',
+                         'X-Requested-With, Content-Type, Accept')
         self.end_headers()
 
+    def parse_url(self, path):
+        path_params = path.split("/")
+        resource = path_params[1]
+
+        # Check if there is a query string parameter
+        if "?" in resource:
+            # GIVEN: /customers?email=jenna@solis.com
+
+            param = resource.split("?")[1]  # email=jenna@solis.com
+            resource = resource.split("?")[0]  # 'customers'
+            pair = param.split("=")  # [ 'email', 'jenna@solis.com' ]
+            key = pair[0]  # 'email'
+            value = pair[1]  # 'jenna@solis.com'
+
+            return (resource, key, value)
+
+        # No query string parameter
+        else:
+            id = None
+
+            try:
+                id = int(path_params[2])
+            except IndexError:
+                pass  # No route parameter exists: /animals
+            except ValueError:
+                pass  # Request had trailing slash: /animals/
+
+            return (resource, id)
+            
     def do_GET(self):
         self._set_headers(200)
 
@@ -39,7 +87,7 @@ class HandleRequests(BaseHTTPRequestHandler):
         # items in it, which means the request was for
         # `/animals` or `/animals/2`
         if len(parsed) == 2:
-            ( resource, id ) = parsed
+            (resource, id) = parsed
 
             if resource == "animals":
                 if id is not None:
@@ -65,7 +113,7 @@ class HandleRequests(BaseHTTPRequestHandler):
         # items in it, which means the request was for
         # `/resource?parameter=value`
         elif len(parsed) == 3:
-            ( resource, key, value ) = parsed
+            (resource, key, value) = parsed
 
             # Is the resource `customers` and was there a
             # query parameter that specified the customer
@@ -76,10 +124,6 @@ class HandleRequests(BaseHTTPRequestHandler):
         self.wfile.write(response.encode())
 
 
-
-
-    # Here's a method on the class that overrides the parent's method.
-    # It handles any PUT request.
     def do_POST(self):
         self._set_headers(201)
         content_len = int(self.headers.get('content-length', 0))
@@ -113,7 +157,7 @@ class HandleRequests(BaseHTTPRequestHandler):
         self.wfile.write(f"{new_customer}".encode())
         self.wfile.write(f"{new_employee}".encode())
         self.wfile.write(f"{new_location}".encode())
-    
+
     def do_DELETE(self):
         # Set a 204 response code
         self._set_headers(204)
@@ -129,7 +173,7 @@ class HandleRequests(BaseHTTPRequestHandler):
         elif resource == "employees":
             delete_employee(id)
         elif resource == "locations":
-            delete_location(id) 
+            delete_location(id)
         # Encode the new animal and send in response
         self.wfile.write("".encode())
 
@@ -145,7 +189,12 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         if resource == "animals":
             success = update_animal(id, post_body)
-        # rest of the elif's
+        elif resource == "customers":
+            success = update_customer(id, post_body)
+        elif resource == "employees":
+            success = update_employee(id, post_body)
+        elif resource == "locations":
+            success = update_location(id, post_body)
 
         if success:
             self._set_headers(204)
@@ -153,36 +202,6 @@ class HandleRequests(BaseHTTPRequestHandler):
             self._set_headers(404)
 
         self.wfile.write("".encode())
-
-
-    def parse_url(self, path):
-        path_params = path.split("/")
-        resource = path_params[1]
-
-        # Check if there is a query string parameter
-        if "?" in resource:
-            # GIVEN: /customers?email=jenna@solis.com
-
-            param = resource.split("?")[1]  # email=jenna@solis.com
-            resource = resource.split("?")[0]  # 'customers'
-            pair = param.split("=")  # [ 'email', 'jenna@solis.com' ]
-            key = pair[0]  # 'email'
-            value = pair[1]  # 'jenna@solis.com'
-
-            return ( resource, key, value )
-
-        # No query string parameter
-        else:
-            id = None
-
-            try:
-                id = int(path_params[2])
-            except IndexError:
-                pass  # No route parameter exists: /animals
-            except ValueError:
-                pass  # Request had trailing slash: /animals/
-
-            return (resource, id)
 
 
 
@@ -194,6 +213,7 @@ def main():
     host = ''
     port = 8088
     HTTPServer((host, port), HandleRequests).serve_forever()
+
 
 if __name__ == "__main__":
     main()
