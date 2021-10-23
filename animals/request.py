0b418b2e1e162627b50e69592c06a8219c8e5d58
Tingting Duan <duan.tingting@yahoo.com>
2021-10-23 11:59:24 -0500

    chapter 14 main completed

--- animals/request.py
+++ animals/request.py
@@ -1,6 +1,6 @@
 import sqlite3
 import json
-from models import Animal
+from models import Animal, Location
 
 # def get_all_animals():
 #     return ANIMALS
@@ -14,14 +14,20 @@ def get_all_animals():
 
         # Write the SQL query to get the information you want
         db_cursor.execute("""
+
         SELECT
             a.id,
             a.name,
             a.breed,
             a.status,
             a.location_id,
-            a.customer_id
-        FROM animal a
+            a.customer_id,
+            l.name location_name,
+            l.address location_address
+        FROM Animal a
+        JOIN Location l
+            ON l.id = a.location_id
+
         """)
 
         # Initialize an empty list to hold all animal representations
@@ -40,6 +46,11 @@ def get_all_animals():
             animal = Animal(row['id'], row['name'], row['breed'],
                             row['status'], row['location_id'],
                             row['customer_id'])
+            # Create a Location instance from the current row
+            location = Location(row['id'], row['location_name'], row['location_address'])
+
+            # Add the dictionary representation of the location to the animal
+            animal.location = location.__dict__
 
             animals.append(animal.__dict__)
 
